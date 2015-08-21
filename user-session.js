00d081d671a6e47ab8f521bb1fc92df6800b6673
garduino <gsa@arsol.net>
2015-08-21 08:42:56 -0300

    Geolocation Examples.

--- user-session.js
+++ user-session.js
@@ -1,11 +1,13 @@
-debug = console.log.bind(console, 'session')
-# debug = (->)
+// Example from https://forums.meteor.com/t/meteor-snippet-catrgory/8542
 
-Accounts.onLogin ({user, connection}) ->
-  userId = user._id
-  start = timestamp()
-  debug('start', userId, start)
-  connection.onClose ->
-    end = timestamp()
-    debug('stop', userId, end)
-    Session.insert({start, stop, userId})
+// debug = console.log.bind(console, 'session')
+// # debug = (->)
+
+// Accounts.onLogin ({user, connection}) ->
+//  userId = user._id
+//  start = timestamp()
+//  debug('start', userId, start)
+//  connection.onClose ->
+//    end = timestamp()
+//    debug('stop', userId, end)
+//    Session.insert({start, stop, userId})
